9ed63fcb25ca5c936370d91ba3e0b54bd7c7e159
yeyoxlab <yeyo.xtremex@gmail.com>
2017-01-19 15:23:26 -0600

    Proyecto

---
+++ js/config/config.js
@@ -0,0 +1,26 @@
+
+
+var config = angular.module("config", []);
+ 
+//realizamos la configuración del enrutado dependiendo de la url
+//con el objeto $routeProvider haciendo uso de when
+//en este caso, cuando estemos en la página principal, le decimos que
+//cargue el archivo templates/index.html y que haga uso del controlador
+//indexController, así en todos los casos
+config.config(function($routeProvider){
+	$routeProvider.when("/", {
+		templateUrl : "templates/index.html",
+		controller : "indexController"
+	})
+	.when("/rest", {
+		templateUrl : "templates/rest.html",
+		controller : "restController"
+	})
+	.when("/api", {
+		templateUrl : "templates/api.html",
+		controller : "apiController"
+	})
+	//este es digamos, al igual que en un switch el default, en caso que 
+	//no hayamos concretado que nos redirija a la página principal
+	.otherwise({ reditrectTo : "/" });
+})
