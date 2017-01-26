53f4fc539384f0bf9f88824e9d2815f728f66be5
Karen CF <cruferk1995@gmail.com>
2017-01-26 09:40:57 -0600

    Translate
    Incrustado librería angular-translate
    Variables translate
    Archivo .json traducciones doc

--- js/config/config.js
+++ js/config/config.js
@@ -1,33 +1,43 @@
 
+var config = angular.module("config", [ 'pascalprecht.translate' ]);
 
-var config = angular.module("config", []);
- 
-//realizamos la configuración del enrutado dependiendo de la url
-//con el objeto $routeProvider haciendo uso de when
-//en este caso, cuando estemos en la página principal, le decimos que
-//cargue el archivo templates/index.html y que haga uso del controlador
-//indexController, así en todos los casos
-config.config(function($routeProvider){
-	$routeProvider.when("/", {
-		templateUrl : "templates/index.html",
-		controller : "indexController"
-	})
-	.when("/rest", {
-		templateUrl : "templates/rest.html",
-		controller : "restController"
-	})
-	.when("/api", {
-		templateUrl : "templates/api.html",
-		controller : "apiController"
-	}).when("/login", {
-		templateUrl : "templates/login.html",
-		controller : "apiController"
-	})
-	.when("/version", {
-		templateUrl : "templates/version.html",
-		controller : "apiController"
-	})
-	//este es digamos, al igual que en un switch el default, en caso que 
-	//no hayamos concretado que nos redirija a la página principal
-	.otherwise({ reditrectTo : "/" });
-})
+// realizamos la configuración del enrutado dependiendo de la url
+// con el objeto $routeProvider haciendo uso de when
+// en este caso, cuando estemos en la página principal, le decimos que
+// cargue el archivo templates/index.html y que haga uso del controlador
+// indexController, así en todos los casos
+config
+		.config(
+				'$translateProvider',
+				function($routeProvider, $translateProvider) {
+					$routeProvider.when("/", {
+						templateUrl : "templates/index.html",
+						controller : "indexController"
+					}).when("/rest", {
+						templateUrl : "templates/rest.html",
+						controller : "restController"
+					}).when("/api", {
+						templateUrl : "templates/api.html",
+						controller : "apiController"
+					}).when("/login", {
+						templateUrl : "templates/login.html",
+						controller : "apiController"
+					}).when("/version", {
+						templateUrl : "templates/version.html",
+						controller : "apiController"
+					})
+					// este es digamos, al igual que en un switch el default, en
+					// caso que
+					// no hayamos concretado que nos redirija a la página
+					// principal
+					.otherwise({
+						reditrectTo : "/"
+					});
+
+					var translations = {
+						TITLE_ONE : "REST APIs",
+						PARAGRAPH_ONE : "Una REST API proporciona acceso mediante programaci\u00F3n para leer y escribir datos de Protcloud. A\u00F1adir un nuevo medidor inteligente, eliminar un medidor inteligente, actualizar un medidor inteligente, leer y escribir informaci\u00F3n del sistema, y m\u00E1s. La REST API identifica aplicaciones PROTCLOUD y los usuarios usan JWE; las respuestas est\u00E1n disponibles en JSON.",
+						TITLE_TWO : "Autenticaci\u00F3n JWE",
+					};
+
+				})
