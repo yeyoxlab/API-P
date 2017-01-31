736c28bfa9eb3245bc6ca128fad6e0e4d96ca8f7
yeyoxlab <yeyo.xtremex@gmail.com>
2017-01-31 17:50:55 -0600

    final- martes

--- js/config/prot_api.js
+++ js/config/prot_api.js
@@ -4,3 +4,12 @@ var prot_api = angular.module("prot_api",['ngRoute','pascalprecht.translate','ng
  
 
 
+prot_api.service('serveData', [function () 
+{
+	return {
+	//Datos almacenados para funcionamiento
+		qty : 'ng-repeat="x in myDataes"',
+		qyp:'ListE.json',
+		lan:"es"
+	};
+}])
--- templates/ListACtrl.js
+++ templates/ListACtrl.js
@@ -1,14 +1,21 @@
+//lista de api
 prot_api.controller("appController", function ($scope, $http) {
-	 $http.get("json/ListA.json").then(function(response) {
+	 $http.get("json/ListA"+$scope.objetoP.lan+".json").then(function(response) {
         $scope.myData = response.data;
+       // console.log($scope.objetoP.lan);
         
     });
+	
+	 
 });
+
+//lista de errores
 prot_api.controller("errorController", function ($scope, $http) {
-	 $http.get("json/ListE.json").then(function(response) {
-        $scope.myData = response.data;
+	 $http.get("json/ListE"+$scope.objetoP.lan+".json").then(function(response) {
+        $scope.myData= response.data;
         
     });
+	 
 });
  
 
