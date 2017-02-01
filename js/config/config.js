c2e67c272f8e6e2c365cf7ac7f2c0a6c493af938
yeyoxlab <yeyo.xtremex@gmail.com>
2017-01-31 18:09:36 -0600

    fun

--- js/config/config.js
+++ js/config/config.js
@@ -6,11 +6,11 @@ prot_api.config(function($routeProvider,$translateProvider,$locationProvider){
 	})
 	.when("/error", {
 		templateUrl : "templates/listE.html",
-		
+		controller : "errorController"
 	})
 	.when("/lista", {
 		templateUrl : "templates/listA.html",
-		controller : "Ctr"		
+		controller : "appController"		
 	}).when("/login", {
 		templateUrl : "templates/login.html",
 		
@@ -33,6 +33,27 @@ prot_api.config(function($routeProvider,$translateProvider,$locationProvider){
 	}).when("/meters_", {
 		templateUrl : "templates/meters_.html",
 		
+	}).when("/meters_b_less", {
+		templateUrl : "templates/meters_b_less.html",
+		
+	}).when("/meters_2", {
+		templateUrl : "templates/meters_2.html",
+	
+	}).when("/meters_b_1", {
+		templateUrl : "templates/meters_b_1.html",
+	
+	}).when("/meters_b_2", {
+		templateUrl : "templates/meters_b_2.html",
+	
+	}).when("/meters_state_relay", {
+		templateUrl : "templates/meters_state_relay.html",
+	
+	}).when("/meters_reset_meter", {
+		templateUrl : "templates/meters_reset_meter.html",
+	
+	}).when("/alarms_startDate_endDate", {
+		templateUrl : "templates/alarms_startDate_endDate.html",
+	
 	})
 	//este es digamos, al igual que en un switch el default, en caso que 
 	//no hayamos concretado que nos redirija a la página principal
